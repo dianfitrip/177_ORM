6cbe5468adf9de19aec978ad34b38825299841c2
dianfitrip <dianfitri683@gmail.com>
2025-10-28 14:30:25 +0700

    membuat komik.js di models

---
+++ models/komik.js
@@ -0,0 +1,22 @@
+module.exports  = (sequelize, DataTypes) => {
+    const Komik = sequelize.define('Komik', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        penulis: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    }, {
+        tableName: 'komik',
+        timestamps: true,
+        freezeTableName: true
+    });
+    return Komik;
+}
